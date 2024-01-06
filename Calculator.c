dcf2dbc0045ee862dfcb947b273f5432d54bf2b9
Yaiphaba1 <151728182+Yaiphaba1@users.noreply.github.com>
2024-01-06 20:20:28 +0530

    Updating Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -57,4 +57,12 @@ float add(float num1, float num2) {
 float subtract(float num1, float num2) {
     return num1 - num2;
 }
+float multiply(float num1, float num2) {
+    return num1 * num2;
+}
+
+float divide(float num1, float num2) {
+    return num1 / num2;
+}
+
 
